e76c0200892195dedffa75109b1b1661a876fd82
Casmo <mathieu@fellicht.nl>
2013-12-31 12:22:16 +0100

    Balancing.

--- three-tower-defense/js/waves.js
+++ three-tower-defense/js/waves.js
@@ -9,21 +9,21 @@ function spawnWave() {
 	maxY = Math.floor(boardSize.z / tileSize) - 1;
 	timeOut = 0;
 	maxMonsters = currentWave * 5;
-	if (maxMonsters > 50) {
-		maxMonsters = 50;
+	if (maxMonsters > 70) {
+		maxMonsters = 70;
 	}
 	someStats = new Object();
-	someStats.hp = Math.round((currentWave+1) * 4.8);
-	if (someStats.hp > 400) {
-		someStats.hp = 400;
+	someStats.hp = Math.round((currentWave+1) * 5.1);
+	if (someStats.hp > 800) {
+		someStats.hp = 800;
 	}
 	someStats.speed = 1;
 	if (someStats.speed > 10) {
 		someStats.speed = 10;
 	}
 	someStats.currency = Math.ceil(currentWave * 0.3);
-	if (someStats.currency > 4) {
-		someStats.currency = 4;
+	if (someStats.currency > 2) {
+		someStats.currency = 2;
 	}
 	timePerMonsterInSeconds = (waveSeconds / maxMonsters) / 3;
 	for (i = 0; i < maxMonsters; i++) {
